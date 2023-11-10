1f9ca4c51622e29178afe22d7ee012500b7cfa3c
ve4r <javierjibran3@gmail.com>
2023-11-10 17:56:33 +0700

    javierwjibran

--- screens/DetailBarang.js
+++ screens/DetailBarang.js
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import { Pressable, View, } from 'react-native'
+import {GluestackUIProvider, Button , Heading, Center, StatusBar, Box, Text, Image, ScrollView, Icon } from "@gluestack-ui/themed";
 import React from 'react'
 
 const DetailBarang = () => {
   return (
-    <View>
-      <Text>DetailBarang</Text>
-    </View>
+    <Box>
+        <Image source={require("../assets/images/getinto2.png")} alt='gambar barang' width={"70%"} />
+        <Text>Nama barang</Text>
+        <Text>Harga barang</Text>
+        <Text>Deskripsi barang</Text>
+
+        <Pressable>
+            <Image source={"../assets/favicon.png"} width={"80%"} bgColor='#aaa' />
+        </Pressable>
+        
+
+
+    </Box>
   )
 }
 
-export default DetailBarang
+export default DetailBarang;
--- screens/EditProfile.js
+++ screens/EditProfile.js
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, } from 'react-native'
+import {GluestackUIProvider, Button, TextButton , Heading, Center, StatusBar, Box, Text, Image, ScrollView, Icon } from "@gluestack-ui/themed";
 import React from 'react'
 
 const EditProfile = () => {
   return (
-    <View>
-      <Text>EditProfile</Text>
-    </View>
+    <Box>
+        <Image source={"../assets/images/anonim.jpg"} width={300} height={600} />
+      <Text>Nama Lengkap</Text>
+      <Text>Alamat Lengkap</Text>
+      <Text>Nomor Hp</Text>
+      <Button><Text>Save</Text></Button>
+    </Box>
   )
 }
 
-export default EditProfile
+export default EditProfile;
