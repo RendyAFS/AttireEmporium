f89203abe8b7bc607706d8917fca655056d60a61
Johanes <anesdiman@gmail.com>
2023-11-11 09:37:40 +0700

    kerangka dan ui statis

--- screens/Create.js
+++ screens/Create.js
@@ -1,12 +1,170 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { useTheme } from "@gluestack-ui/themed";
 
 const Create = () => {
-  return (
-    <View>
-      <Text>Create</Text>
-    </View>
-  )
-}
+  // State untuk menyimpan informasi kostum yang akan diposting
+  const [costumeName, setCostumeName] = useState('');
+  const [costumeDescription, setCostumeDescription] = useState('');
+  const [rentalPrice, setRentalPrice] = useState('');
+  const [costumeImages, setCostumeImages] = useState([]);
 
-export default Create
+  // Fungsi untuk menangani proses posting kostum
+  const handlePostCostume = () => {
+    // Simulasi posting kostum (ganti dengan logika sesungguhnya)
+    console.log('Posting costume:', {
+      costumeName,
+      costumeDescription,
+      rentalPrice,
+      costumeImages,
+    });
+
+    // Reset nilai form setelah posting
+    setCostumeName('');
+    setCostumeDescription('');
+    setRentalPrice('');
+    setCostumeImages([]);
+  };
+
+  // Fungsi untuk menangani pemilihan gambar kostum
+  const handleImageSelection = () => {
+    // Simulasi pemilihan gambar (belum ada logika sesungguhnya)
+    console.log('Selecting image...');
+  };
+
+  // Fungsi untuk membuka drawer (menu samping)
+  const openDrawer = () => {
+    // Simulasi membuka drawer (belum ada logika navigasi sesungguhnya)
+    console.log('Opening drawer...');
+  };
+
+  // Access the theme
+  const theme = useTheme();
+
+  // Define the styles
+  const styles = {
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 16,
+      backgroundColor: '#f5f5f5',
+    },
+    menuButton: {
+      marginRight: 16,
+    },
+    menuIcon: {
+      fontSize: 24,
+      color: '#DF9B52',
+    },
+    headerTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#DF9B52',
+    },
+    content: {
+      padding: 16,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 8,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#ddd',
+      borderRadius: 4,
+      padding: 8,
+      marginBottom: 16,
+    },
+    multilineInput: {
+      height: 80,
+    },
+    imageThumbnail: {
+      width: 100,
+      height: 100,
+      marginRight: 8,
+    },
+    imageButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 50,
+      borderRadius: 4,
+      backgroundColor: '#DF9B52',
+      marginBottom: 16,
+    },
+    imageButtonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    postButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 50,
+      borderRadius: 4,
+      backgroundColor: '#DF9B52',
+    },
+    postButtonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+  };
+
+  return (
+    <View style={{ ...styles.container, padding: 16 }}>
+      <View style={{ ...styles.header, backgroundColor: 'black' }}>
+        {/* Tombol untuk membuka drawer */}
+        <TouchableOpacity onPress={openDrawer} style={styles.menuButton}>
+          <Text style={styles.menuIcon}>☰</Text>
+        </TouchableOpacity>
+        {/* Judul halaman */}
+        <Text style={styles.headerTitle}>Post Your Costume</Text>
+        <View style={{ flex: 1 }}></View>
+      </View>
+      {/* Konten halaman */}
+      <ScrollView contentContainerStyle={styles.content}>
+        {/* Bagian Detail Kostum */}
+        <Text style={styles.title}>Detail Kostum</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nama Kostum"
+          value={costumeName}
+          onChangeText={(text) => setCostumeName(text)}
+        />
+        <TextInput
+          style={[styles.input, styles.multilineInput]}
+          placeholder="Deskripsi"
+          value={costumeDescription}
+          onChangeText={(text) => setCostumeDescription(text)}
+          multiline
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Harga Rental (per hari)"
+          value={rentalPrice}
+          onChangeText={(text) => setRentalPrice(text)}
+          keyboardType="numeric"
+        />
+        {/* Bagian Gambar Kostum */}
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {costumeImages.map((image, index) => (
+            <Image key={index} source={{ uri: image }} style={styles.imageThumbnail} />
+          ))}
+        </ScrollView>
+        {/* Tombol untuk menambahkan gambar kostum */}
+        <TouchableOpacity style={styles.imageButton} onPress={handleImageSelection}>
+          <Text style={styles.imageButtonText}>Tambahkan Gambar</Text>
+        </TouchableOpacity>
+        {/* Tombol untuk memposting kostum */}
+        <TouchableOpacity style={styles.postButton} onPress={handlePostCostume}>
+          <Text style={styles.postButtonText}>Post Costume</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Create;
