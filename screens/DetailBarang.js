f05d10ed2c9ddee390955e65c92c471fe44e1634
RendyAFS <arendy724@gmail.com>
2023-11-12 19:41:43 +0700

    fix ukuran japir

--- screens/DetailBarang.js
+++ screens/DetailBarang.js
@@ -4,6 +4,7 @@ import { Pressable, Alert } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from '@expo/vector-icons'; 
 
+// Functional
 const DetailBarang = ({ route }) => {
   const navigation = useNavigation();
   const data = route.params.item;
