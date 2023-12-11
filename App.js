4703d391888012af0544b45dc7a82b2568d93aa8
RendyAFS <arendy724@gmail.com>
2023-12-11 22:30:18 +0700

    update front end Tabs, History, Form penyewaan dan pengembalian

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { GluestackUIProvider, Heading, Center, StatusBar, Box, Text, } from "@gluestack-ui/themed";
+import { GluestackUIProvider, Text, } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 import { Ionicons, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { Home, History, Favorite, Profile, FormPenyewaan, FormPengembalian, Register, Login, Katalog, GetInto1, GetInto2, DetailBarang, EditProfile, CreateItem, EditItem, ProfileRenter, Test } from "./screens/";
@@ -17,43 +17,57 @@ const Tabs = () => {
         tabBarIcon: ({ focused, color }) => {
           let iconName;
           switch (route.name) {
-            case "Home":
-              iconName = focused ? "shopping" : "shopping-outline";
+            case 'Home':
+              iconName = focused ? 'shopping' : 'shopping-outline';
               return (
-                <MaterialCommunityIcons name={iconName} size={28} color={focused ? "#DF9B52" : color} />
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={28}
+                  color={focused ? '#021C35' : color}
+                />
               );
-            case "Favorite":
-              iconName = focused ? "favorite" : "favorite-outline";
+            case 'Favorite':
+              iconName = focused ? 'favorite' : 'favorite-outline';
               return (
-                <MaterialIcons name={iconName} size={28} color={focused ? "#DF9B52" : color} />
+                <MaterialIcons name={iconName} size={28} color={focused ? '#021C35' : color} />
               );
-            case "History":
-              iconName = focused ? "history" : "history";
+            case 'History':
+              iconName = focused ? 'history' : 'history';
               return (
-                <MaterialCommunityIcons name={iconName} size={28} color={focused ? "#DF9B52" : color} />
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={28}
+                  color={focused ? '#021C35' : color}
+                />
               );
-            case "Profile":
-              iconName = focused ? "person-circle-sharp" : "person-circle-outline";
+            case 'Profile':
+              iconName = focused ? 'person-circle-sharp' : 'person-circle-outline';
               return (
-                <Ionicons name={iconName} size={28} color={focused ? "#DF9B52" : color} />
-              )
+                <Ionicons name={iconName} size={28} color={focused ? '#021C35' : color} />
+              );
+            default:
+              return null;
           }
         },
-        tabBarIconStyle: { marginTop: 6 },
+        tabBarIconStyle: {
+          marginTop: 6,
+        },
         tabBarStyle: {
+          position: 'absolute',
+          width: '80%',
+          marginHorizontal: '10%',
+          marginBottom: 20,
           height: 70,
-          borderTopWidth: 1,
-          backgroundColor: "#313C47",
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
+          borderWidth: 2,
+          borderColor: '#021C35',
+          backgroundColor: '#fff',
+          borderRadius: 10,
         },
-        tabBarLabel: ({ children, color, focused }) => {
-          return (
-            <Text fontSize={13} color={focused ? "#DF9B52" : color} mb={2}>
-              {children}
-            </Text>
-          );
-        },
+        tabBarLabel: ({ children, color, focused }) => (
+          <Text style={{ fontSize: 13, color: focused ? '#021C35' : color, marginBottom: 2 }}>
+            {children}
+          </Text>
+        ),
       })}
     >
       <Tab.Screen name="Home" component={Home} options={noHead} />
@@ -69,14 +83,14 @@ const App = () => {
     <GluestackUIProvider config={config}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="GetInto1">
-          <Stack.Screen name="Tabs" component={Tabs} options={{ ...noHead, statusBarColor: '#DF9B52' }} />
-          <Stack.Screen name="FormPenyewaan" component={FormPenyewaan} options={{ ...noHead, statusBarColor: '#DF9B52', statusBarStyle: 'white' }} />
-          <Stack.Screen name="FormPengembalian" component={FormPengembalian} options={{ ...noHead, statusBarColor: '#DF9B52', statusBarStyle: 'white' }} />
-          <Stack.Screen name="Register" component={Register} options={{ ...noHead, statusBarColor: '#021C35' }} />
-          <Stack.Screen name="Login" component={Login} options={{ ...noHead, statusBarColor: '#021C35' }} />
+          <Stack.Screen name="Tabs" component={Tabs} options={{ ...noHead, statusBarColor: 'white', statusBarStyle: 'dark' }} />
+          <Stack.Screen name="FormPenyewaan" component={FormPenyewaan} options={{title: 'Penyewaan barang', statusBarColor: 'white', statusBarStyle: 'dark' }} />
+          <Stack.Screen name="FormPengembalian" component={FormPengembalian} options={{title: 'Pengembalian barang', statusBarColor: 'white', statusBarStyle: 'dark' }}/>
+          <Stack.Screen name="Register" component={Register} options={{ ...noHead, statusBarColor: 'white', statusBarStyle: 'dark' }} />
+          <Stack.Screen name="Login" component={Login} options={{ ...noHead, statusBarColor: 'white', statusBarStyle: 'dark' }} />
           <Stack.Screen name="Katalog" component={Katalog} options={{ ...noHead, statusBarColor: 'white', statusBarStyle: 'dark' }} />
-          <Stack.Screen name="GetInto1" component={GetInto1} options={{ ...noHead, statusBarColor: '#eee', statusBarStyle: 'dark' }} />
-          <Stack.Screen name="GetInto2" component={GetInto2} options={{ ...noHead, statusBarColor: '#eee', statusBarStyle: 'dark' }} />
+          <Stack.Screen name="GetInto1" component={GetInto1} options={{ ...noHead, statusBarColor: 'white', statusBarStyle: 'dark' }} />
+          <Stack.Screen name="GetInto2" component={GetInto2} options={{ ...noHead, statusBarColor: 'white', statusBarStyle: 'dark' }} />
           <Stack.Screen name="DetailBarang" component={DetailBarang} options={{ statusBarColor: 'white', statusBarStyle: 'dark' }} />
           <Stack.Screen name="Edit Profile" component={EditProfile} options={{ headerStyle: { backgroundColor: "#021C35" }, headerTintColor: 'white', statusBarColor: '#021C35' }} />
           <Stack.Screen name="Create Item" component={CreateItem} options={{ title: 'Tambah Item', statusBarColor: 'white', statusBarStyle: 'dark' }} />
