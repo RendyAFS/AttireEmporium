848210f2433d4c9b0736c6f06606d3ec85108d9c
Dennydaffa <dennydaffarizaldy16@gmail.com>
2023-12-18 17:41:26 +0700

    CRUD CLEAR COYYY -gambar tapi

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Box, Image, Button, Heading, Text, Pressable, } from "@gluestack-ui/themed";
+import { Box, Image, Button, Heading, Text, Pressable, Modal, ModalBackdrop, ModalContent, ModalHeader, ModalBody, ModalFooter, ButtonText, ModalCloseButton } from "@gluestack-ui/themed";
 import { Alert } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from '@expo/vector-icons';
-
-// Functional
+import { useState, useRef } from 'react';
+import firebase from "../firebase";
 const Detail = ({ route }) => {
     const navigation = useNavigation();
     const data = route.params.item;
+    const [showModal, setShowModal] = useState(false)
+    const ref = React.useRef(null)
+
+    const deleteCostume = async () => {
+        try {
+
+            const costumeRef = firebase.database().ref(`costumes/${data.costumeId}`);
+            const snapshot = await costumeRef.once("value");
+            const existingCostume = snapshot.val();
+
+            if (!existingCostume) {
+                console.log("Costume not found");
+                return;
+            }
+
+            // Hapus catatan dari database
+            await costumeRef.remove();
+            console.log("Note deleted successfully");
+            navigation.replace("Profile Renter");
+        } catch (error) {
+            throw error;
+        }
+    };
     console.log(data);
 
     return (
@@ -36,21 +59,66 @@ const Detail = ({ route }) => {
 
             <Box width={"100%"} alignItems='center' backgroundColor='transparent' paddingBottom={20} paddingTop={10}>
                 <Pressable onPress={() => navigation.navigate('Edit Item', { data: data })} >
-                    <Box  width={350} backgroundColor='#021C35' p={10} alignItems='center' borderRadius={10}>
+                    <Box width={350} backgroundColor='#021C35' p={10} alignItems='center' borderRadius={10}>
                         <Text color='white' fontWeight='bold'  >
                             Ubah Barang
                         </Text>
                     </Box>
 
                 </Pressable>
-                <Pressable onPress={() => navigation.navigate('FormPenyewaan', { data: data })} >
-                    <Box  width={350} backgroundColor='#DF5252' p={10} alignItems='center' borderRadius={10}>
+                <Pressable onPress={() => setShowModal(true)} ref={ref} >
+                    <Box width={350} backgroundColor='#DF5252' mt={2} p={10} alignItems='center' borderRadius={10}>
                         <Text color='white' fontWeight='bold' >
-                           Hapus Barang
+                            Hapus Barang
                         </Text>
                     </Box>
                 </Pressable>
             </Box>
+            <Modal
+                isOpen={showModal}
+                onClose={() => {
+                    setShowModal(false)
+                }}
+                finalFocusRef={ref}
+            >
+                <ModalBackdrop />
+                <ModalContent>
+                    <ModalHeader>
+                        <Heading size="lg">Apakah anda ingin hapus item ini?</Heading>
+                        <ModalCloseButton>
+                            {/* <Icon as={CloseIcon} /> */}
+                        </ModalCloseButton>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Text>
+                            {data.costumeName}
+                        </Text>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            variant="outline"
+                            size="sm"
+                            action="secondary"
+                            mr="$3"
+                            onPress={() => {
+                                setShowModal(false)
+                            }}
+                        >
+                            <ButtonText>Cancel</ButtonText>
+                        </Button>
+                        <Button
+                            size="sm"
+                            action="positive"
+                            borderWidth="$0"
+                            backgroundColor={'$red500'}
+                            onPress={deleteCostume
+                            }
+                        >
+                            <ButtonText>Delete</ButtonText>
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box >
     );
 }
